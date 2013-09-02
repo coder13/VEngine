be00d1a46a1f68c160df095de78cd29c6ab1baa1
coder13 <choover11@gmail.com>
2013-09-02 13:58:39 -0700

    Prettied up Vec3 and Vec2

--- src/com/virtuel/math/vec/Vec2.java
+++ src/com/virtuel/math/vec/Vec2.java
@@ -7,46 +7,47 @@ public abstract class Vec2<N> {
 	public static class i extends Vec2<Integer> {
 
 		public i() {
-			super(0,0);
-		}
-		
-		public i(int x, int y) {
-			super(x, y);
-			
+			super(0, 0);
 		}
 
-		public i(int xy){
+		public i(int x, int y) {
+			super(x, y);
+
+		}
+
+		public i(int xy) {
 			super(xy);
 		}
-		
+
 		@Override
 		public Vec2.i scale(Integer a) {
-			return new Vec2.i(X*a, Y*a);
+			return new Vec2.i(X * a, Y * a);
 		}
 
 		@Override
 		public Vec2.i add(Vec2<Integer> vec) {
-			return new Vec2.i(X+vec.X, Y+vec.Y);
+			return new Vec2.i(X + vec.X, Y + vec.Y);
 		}
 
 		@Override
 		public Vec2.i sub(Vec2<Integer> vec) {
-			return new Vec2.i(X-vec.X, Y-vec.Y);
+			return new Vec2.i(X - vec.X, Y - vec.Y);
 		}
 
 		@Override
 		public Vec2.i mul(Vec2<Integer> vec) {
-			return new Vec2.i(X*vec.X, Y*vec.Y);
+			return new Vec2.i(X * vec.X, Y * vec.Y);
 		}
 
 		@Override
 		public Vec2.i div(Vec2<Integer> vec) {
-			return new Vec2.i(X/vec.X, Y/vec.Y);
+			return new Vec2.i(X / vec.X, Y / vec.Y);
 		}
 
 		@Override
 		public Integer dist(Vec2<Integer> vec) {
-			return (int) VMath.sqrt((X-vec.X)*(X-vec.X) + (Y-vec.Y)*(Y-vec.Y));
+			return (int) VMath.sqrt((X - vec.X) * (X - vec.X) + (Y - vec.Y)
+					* (Y - vec.Y));
 		}
 
 		@Override
@@ -54,76 +55,77 @@ public abstract class Vec2<N> {
 			return null;
 		}
 
-		public Vec2.i cross(Vec2<Integer> vec){
-			return new Vec2.i(Y*vec.X - X*vec.Y, Y*vec.X - X*vec.Y);
-		}	
+		public Vec2.i cross(Vec2<Integer> vec) {
+			return new Vec2.i(Y * vec.X - X * vec.Y, Y * vec.X - X * vec.Y);
+		}
 
 		@Override
 		public Vec2.i normal() {
 			int mag = mag();
-			return new Vec2.i(X/mag, Y/mag);
+			return new Vec2.i(X / mag, Y / mag);
 		}
-				
+
 		@Override
 		public Integer mag() {
-			return X*X+Y*Y;
+			return X * X + Y * Y;
 		}
-		
+
 		@Override
 		public Integer length() {
-			return (int) VMath.sqrt(X*X+Y*Y);
+			return (int) VMath.sqrt(X * X + Y * Y);
 		}
-		
-		public long combine(){
-			long x = (long)(X<0?(long)X&0x7fffffff:X),
-				 y = (long)(Y<0?(long)Y&0x7fffffff:Y);
+
+		public long combine() {
+			long x = (long) (X < 0 ? (long) X & 0x7fffffff : X), y = (long) (Y < 0 ? (long) Y & 0x7fffffff
+					: Y);
 			return x | y << 32;
 		}
-		
+
 	}
-	
+
 	public static class f extends Vec2<Float> {
 
-		public f(){
+		public f() {
 			super(0.0f, 0.0f);
 		}
-		
+
 		public f(float x, float y) {
 			super(x, y);
 		}
 
-		public f(float xy){
+		public f(float xy) {
 			super(xy);
 		}
-		
+
 		@Override
 		public Vec2.f scale(Float a) {
-			return new Vec2.f(X*a, Y*a);
+			return new Vec2.f(X * a, Y * a);
 		}
 
 		@Override
 		public Vec2.f add(Vec2<Float> vec) {
-			return new Vec2.f(X+vec.X, Y+vec.Y);
+			return new Vec2.f(X + vec.X, Y + vec.Y);
 		}
 
 		@Override
 		public Vec2.f sub(Vec2<Float> vec) {
-			return new Vec2.f(X-vec.X, Y-vec.Y);
+			return new Vec2.f(X - vec.X, Y - vec.Y);
 		}
 
 		@Override
 		public Vec2.f mul(Vec2<Float> vec) {
-			return new Vec2.f(X*vec.X, Y*vec.Y);
+			return new Vec2.f(X * vec.X, Y * vec.Y);
 		}
 
 		@Override
 		public Vec2.f div(Vec2<Float> vec) {
-			return new Vec2.f(X/vec.X, Y/vec.Y);
+			return new Vec2.f(X / vec.X, Y / vec.Y);
 		}
 
 		@Override
 		public Float dist(Vec2<Float> vec) {
-			return (float) VMath.sqrt((X-vec.X)*(X-vec.X) + (Y-vec.Y)*(Y-vec.Y));
+			return (float) VMath.sqrt((X - vec.X) * (X - vec.X) + (Y - vec.Y)
+					* (Y - vec.Y));
 		}
 
 		@Override
@@ -131,158 +133,156 @@ public abstract class Vec2<N> {
 			return null;
 		}
 
-		public Vec2.f cross(Vec2<Float> vec){
-			return new Vec2.f(Y*vec.X - X*vec.Y, Y*vec.X - X*vec.Y);
+		public Vec2.f cross(Vec2<Float> vec) {
+			return new Vec2.f(Y * vec.X - X * vec.Y, Y * vec.X - X * vec.Y);
 		}
 
 		@Override
 		public Vec2.f normal() {
 			float mag = mag();
-			return new Vec2.f(X/mag, Y/mag);
+			return new Vec2.f(X / mag, Y / mag);
 		}
-				
+
 		@Override
 		public Float mag() {
-			return X*X+Y*Y;
+			return X * X + Y * Y;
 		}
-		
+
 		@Override
 		public Float length() {
-			return (float) VMath.sqrt(X*X + Y*Y);
+			return (float) VMath.sqrt(X * X + Y * Y);
 		}
-		
+
 	}
 
 	public static class d extends Vec2<Double> {
 
-		public d(){
+		public d() {
 			super(0.0, 0.0);
 		}
-		
+
 		public d(double x, double y) {
 			super(x, y);
 		}
 
-		public d(double xy){
+		public d(double xy) {
 			super(xy);
 		}
-		
+
 		@Override
 		public Vec2.d scale(Double a) {
-			return new Vec2.d(X*a, Y*a);
+			return new Vec2.d(X * a, Y * a);
 		}
 
 		@Override
 		public Vec2.d add(Vec2<Double> vec) {
-			return new Vec2.d(X+vec.X, Y+vec.Y);
+			return new Vec2.d(X + vec.X, Y + vec.Y);
 		}
 
 		@Override
 		public Vec2.d sub(Vec2<Double> vec) {
-			return new Vec2.d(X-vec.X, Y-vec.Y);
+			return new Vec2.d(X - vec.X, Y - vec.Y);
 		}
 
 		@Override
 		public Vec2.d mul(Vec2<Double> vec) {
-			return new Vec2.d(X*vec.X, Y*vec.Y);
+			return new Vec2.d(X * vec.X, Y * vec.Y);
 		}
 
 		@Override
 		public Vec2.d div(Vec2<Double> vec) {
-			return new Vec2.d(X/vec.X, Y/vec.Y);
+			return new Vec2.d(X / vec.X, Y / vec.Y);
 		}
 
 		@Override
 		public Double dist(Vec2<Double> vec) {
-			return (double) VMath.sqrt((X-vec.X)*(X-vec.X) + (Y-vec.Y)*(Y-vec.Y));
+			return (double) VMath.sqrt((X - vec.X) * (X - vec.X) + (Y - vec.Y)
+					* (Y - vec.Y));
 		}
 
 		@Override
 		public Double dot(Vec2<Double> vec) {
-			return X*vec.X + Y*vec.Y;
+			return X * vec.X + Y * vec.Y;
 		}
 
 		public Vec2.d cross(Vec2<Double> vec) {
-			return new Vec2.d(Y*vec.X - X*vec.Y, Y*vec.X - X*vec.Y);
+			return new Vec2.d(Y * vec.X - X * vec.Y, Y * vec.X - X * vec.Y);
 		}
 
 		@Override
 		public Vec2.d normal() {
 			double mag = mag();
-			return new Vec2.d(X/mag, Y/mag);
+			return new Vec2.d(X / mag, Y / mag);
 		}
-		
+
 		public Vec2.d normalize() {
-			return new Vec2.d(1.0/X, 1.0/Y);
+			return new Vec2.d(1.0 / X, 1.0 / Y);
 		}
-				
+
 		@Override
 		public Double mag() {
-			return X*X+Y*Y;
+			return X * X + Y * Y;
 		}
-		
+
 		@Override
 		public Double length() {
-			return (double) VMath.sqrt(X*X + Y*Y);
+			return (double) VMath.sqrt(X * X + Y * Y);
 		}
-		
+
 	}
-	
-	
+
 	public N X;
 	public N Y;
-	
+
 	public Vec2() {
 	}
-	
-	public Vec2(N x, N y){
+
+	public Vec2(N x, N y) {
 		X = x;
 		Y = y;
 	}
 
-	public Vec2(N xy){
+	public Vec2(N xy) {
 		X = xy;
 		Y = xy;
 	}
-	
-	public void set(N x, N y){
+
+	public void set(N x, N y) {
 		X = x;
 		Y = y;
 	}
-	
-	/** Multiplies both X, Y and Z by a. */
+
+	/** Multiplies both X, Y by a. */
 	public abstract Vec2<N> scale(N a);
-	
+
 	public abstract Vec2<N> add(Vec2<N> vec);
 
 	public abstract Vec2<N> sub(Vec2<N> vec);
-	
+
 	public abstract Vec2<N> mul(Vec2<N> vec);
-	
+
 	public abstract Vec2<N> div(Vec2<N> vec);
-	
+
 	public abstract N dist(Vec2<N> vec);
-	
+
 	public abstract N dot(Vec2<N> vec);
-	
+
 	public abstract Vec2<N> cross(Vec2<N> vec);
-	
+
 	public abstract Vec2<N> normal();
-	
+
 	public abstract N mag();
-		
+
 	public abstract N length();
 
-	
 	@Override
-	public int hashCode(){
+	public int hashCode() {
 		return 0;
 	}
 
-		
 	@Override
-	public boolean equals(Object obj){
-		Vec2<?> vec = (obj instanceof Vec2<?> ? (Vec2<?>)obj : null);
+	public boolean equals(Object obj) {
+		Vec2<?> vec = (obj instanceof Vec2<?> ? (Vec2<?>) obj : null);
 		return (vec == null ? null : (X == vec.X && Y == vec.Y));
 	}
 
@@ -290,14 +290,13 @@ public abstract class Vec2<N> {
 	public String toString() {
 		return String.format("[%s, %s]", X, Y);
 	}
-	
 
-	public static Vec2.i floor(Vec2.d vec){
-		return new Vec2.i((int)VMath.floor(vec.X), (int)VMath.floor(vec.Y));
+	public static Vec2.i floor(Vec2.d vec) {
+		return new Vec2.i((int) VMath.floor(vec.X), (int) VMath.floor(vec.Y));
 	}
-	
-	public static Vec2.i floor(Vec3.f vec){
-		return new Vec2.i((int)VMath.floor(vec.X), (int)VMath.floor(vec.Y));
+
+	public static Vec2.i floor(Vec3.f vec) {
+		return new Vec2.i((int) VMath.floor(vec.X), (int) VMath.floor(vec.Y));
 	}
-	
+
 }
--- src/com/virtuel/math/vec/Vec3.java
+++ src/com/virtuel/math/vec/Vec3.java
@@ -12,79 +12,78 @@ public abstract class Vec3<N> {
 			Y = 0;
 			Z = 0;
 		}
-		
+
 		public i(int x, int y, int z) {
 			super(x, y, z);
-			
+
 		}
 
 		@Override
 		public Vec3.i scale(Integer a) {
-			return new Vec3.i(X*a, Y*a, Z*a);
+			return new Vec3.i(X * a, Y * a, Z * a);
 		}
 
 		@Override
 		public Vec3.i add(Vec3<Integer> vec) {
-			return new Vec3.i(X-vec.X, Y-vec.Y, Z-vec.Z);
+			return new Vec3.i(X - vec.X, Y - vec.Y, Z - vec.Z);
 		}
 
 		@Override
 		public Vec3.i sub(Vec3<Integer> vec) {
-			return new Vec3.i(X*vec.X, Y*vec.Y, Z*vec.Z);
+			return new Vec3.i(X * vec.X, Y * vec.Y, Z * vec.Z);
 		}
 
 		@Override
 		public Vec3.i mul(Vec3<Integer> vec) {
-			return new Vec3.i(X*vec.X, Y*vec.Y, Z*vec.Z);
+			return new Vec3.i(X * vec.X, Y * vec.Y, Z * vec.Z);
 		}
 
 		@Override
 		public Vec3.i div(Vec3<Integer> vec) {
-			return new Vec3.i(X/vec.X, Y/vec.Y, Z/vec.Z);
+			return new Vec3.i(X / vec.X, Y / vec.Y, Z / vec.Z);
 		}
 
 		@Override
 		public Integer dist(Vec3<Integer> vec) {
-			return (int) VMath.sqrt((X-vec.X)*(X-vec.X) + (Y-vec.Y)*(Y-vec.Y) + (Z-vec.Z)*(Z-vec.Z));
+			return (int) VMath.sqrt((X - vec.X) * (X - vec.X) + (Y - vec.Y)
+					* (Y - vec.Y) + (Z - vec.Z) * (Z - vec.Z));
 		}
 
 		@Override
 		public Integer dot(Vec3<Integer> vec) {
-			return X*vec.X + Y*vec.Y + Z*vec.Z;
+			return X * vec.X + Y * vec.Y + Z * vec.Z;
 		}
 
 		@Override
 		public Vec3.i cross(Vec3<Integer> vec) {
-            return new Vec3.i(Y*vec.Z - Z*vec.Y, Z*vec.X - X*vec.Z, X*vec.Y - Y*vec.X);
+			return new Vec3.i(Y * vec.Z - Z * vec.Y, Z * vec.X - X * vec.Z, X
+					* vec.Y - Y * vec.X);
 		}
-		
+
 		public Vec3.i normal() {
 			int mag = mag();
-			return new Vec3.i(X/mag, Y/mag, Z/mag);
+			return new Vec3.i(X / mag, Y / mag, Z / mag);
 		}
-		
+
 		@Override
 		public Integer mag() {
-			return X*X + Y*Y + Z*Z;
+			return X * X + Y * Y + Z * Z;
 		}
-		
+
 		@Override
 		public Integer length() {
 			return (int) VMath.sqrt(mag());
 		}
-		
-		
-		
-		public long combine(){
-			long x = (long)(X<0?(long)X&0x7fffff:X),
-				 y = (long)(Y<0?(long)Y&0x7fff  :Y),
-				 z = (long)(Z<0?(long)Z&0x7fffff:Z);
+
+		public long combine() {
+			long x = (long) (X < 0 ? (long) X & 0x7fffff : X), y = (long) (Y < 0 ? (long) Y & 0x7fff
+					: Y), z = (long) (Z < 0 ? (long) Z & 0x7fffff : Z);
 			return x << 48 | z << 24 | y;
 		}
-	
-		@Override 
+
+		@Override
 		public int hashCode() {
-			return (int)combine();
+			return (int) combine();
 		}
 
 		@Override
@@ -92,71 +91,73 @@ public abstract class Vec3<N> {
 			return new Vec3.i(X, Y, Z);
 		}
 	}
-	
+
 	public static class f extends Vec3<Float> {
 
-		public f(){
+		public f() {
 			super();
 			X = 0.0f;
 			Y = 0.0f;
 			Z = 0.0f;
 		}
-		
+
 		public f(float x, float y, float z) {
 			super(x, y, z);
 		}
 
 		@Override
 		public Vec3.f scale(Float a) {
-			return new Vec3.f(X*a, Y*a, Z*a);
+			return new Vec3.f(X * a, Y * a, Z * a);
 		}
 
 		@Override
 		public Vec3.f add(Vec3<Float> vec) {
-			return new Vec3.f(X+vec.X, Y+vec.Y, Z+vec.Z);
+			return new Vec3.f(X + vec.X, Y + vec.Y, Z + vec.Z);
 		}
 
 		@Override
 		public Vec3.f sub(Vec3<Float> vec) {
-			return new Vec3.f(X-vec.X, Y-vec.Y, Z-vec.Z);
+			return new Vec3.f(X - vec.X, Y - vec.Y, Z - vec.Z);
 		}
 
 		@Override
 		public Vec3.f mul(Vec3<Float> vec) {
-			return new Vec3.f(X*vec.X, Y*vec.Y, Z*vec.Z);
+			return new Vec3.f(X * vec.X, Y * vec.Y, Z * vec.Z);
 		}
 
 		@Override
 		public Vec3.f div(Vec3<Float> vec) {
-			return new Vec3.f(X/vec.X, Y/vec.Y, Z/vec.Z);
+			return new Vec3.f(X / vec.X, Y / vec.Y, Z / vec.Z);
 		}
 
 		@Override
 		public Float dist(Vec3<Float> vec) {
-			return (float) VMath.sqrt((X-vec.X)*(X-vec.X) + (Y-vec.Y)*(Y-vec.Y) + (Z-vec.Z)*(Z-vec.Z));
+			return (float) VMath.sqrt((X - vec.X) * (X - vec.X) + (Y - vec.Y)
+					* (Y - vec.Y) + (Z - vec.Z) * (Z - vec.Z));
 		}
 
 		@Override
 		public Float dot(Vec3<Float> vec) {
-			return X*vec.X + Y*vec.Y + Z*vec.Z;
+			return X * vec.X + Y * vec.Y + Z * vec.Z;
 		}
 
 		@Override
-		public Vec3.f cross(Vec3<Float> vec){
-			return new Vec3.f(Y*vec.Z - Z*vec.Y, Z*vec.X - X*vec.Z, X*vec.Y - Y*vec.X);
+		public Vec3.f cross(Vec3<Float> vec) {
+			return new Vec3.f(Y * vec.Z - Z * vec.Y, Z * vec.X - X * vec.Z, X
+					* vec.Y - Y * vec.X);
 		}
-		
+
 		@Override
 		public Vec3.f normal() {
 			float mag = mag();
-			return new Vec3.f(X/mag, Y/mag, Z/mag);
+			return new Vec3.f(X / mag, Y / mag, Z / mag);
 		}
-				
+
 		@Override
 		public Float mag() {
-			return X*X + Y*Y + Z*Z;
+			return X * X + Y * Y + Z * Z;
 		}
-		
+
 		@Override
 		public Float length() {
 			return (float) VMath.sqrt(mag());
@@ -166,153 +167,161 @@ public abstract class Vec3<N> {
 		public Vec3.f clone() {
 			return new Vec3.f(X, Y, Z);
 		}
-		
+
 	}
 
 	public static class d extends Vec3<Double> {
 
-		public d(){
+		public d() {
 			super();
 			X = 0.0;
 			Y = 0.0;
 			Z = 0.0;
 		}
-		
+
 		public d(double x, double y, double z) {
 			super(x, y, z);
 		}
 
 		@Override
 		public Vec3.d scale(Double a) {
-			return new Vec3.d(X*a, Y*a, Z*a);
+			return new Vec3.d(X * a, Y * a, Z * a);
 		}
 
 		@Override
 		public Vec3.d add(Vec3<Double> vec) {
-			return new Vec3.d(X+vec.X, Y+vec.Y, Z+vec.Z);
+			return new Vec3.d(X + vec.X, Y + vec.Y, Z + vec.Z);
 		}
 
 		@Override
 		public Vec3.d sub(Vec3<Double> vec) {
-			return new Vec3.d(X-vec.X, Y-vec.Y, Z-vec.Z);
+			return new Vec3.d(X - vec.X, Y - vec.Y, Z - vec.Z);
 		}
 
 		@Override
 		public Vec3.d mul(Vec3<Double> vec) {
-			return new Vec3.d(X*vec.X, Y*vec.Y, Z*vec.Z);
+			return new Vec3.d(X * vec.X, Y * vec.Y, Z * vec.Z);
 		}
 
 		@Override
 		public Vec3.d div(Vec3<Double> vec) {
-			return new Vec3.d(X/vec.X, Y/vec.Y, Z/vec.Z);
+			return new Vec3.d(X / vec.X, Y / vec.Y, Z / vec.Z);
 		}
 
 		@Override
 		public Double dist(Vec3<Double> vec) {
-			return (double) VMath.sqrt((X-vec.X)*(X-vec.X) + (Y-vec.Y)*(Y-vec.Y) + (Z-vec.Z)*(Z-vec.Z));
+			return (double) VMath.sqrt((X - vec.X) * (X - vec.X) + (Y - vec.Y)
+					* (Y - vec.Y) + (Z - vec.Z) * (Z - vec.Z));
 		}
 
 		@Override
 		public Double dot(Vec3<Double> vec) {
-			return X*vec.X + Y*vec.Y + Z*vec.Z;
+			return X * vec.X + Y * vec.Y + Z * vec.Z;
 		}
 
 		@Override
-		public Vec3.d cross(Vec3<Double> vec){
-			return new Vec3.d(Y*vec.Z - Z*vec.Y, Z*vec.X - X*vec.Z, X*vec.Y - Y*vec.X);
+		public Vec3.d cross(Vec3<Double> vec) {
+			return new Vec3.d(Y * vec.Z - Z * vec.Y, Z * vec.X - X * vec.Z, X
+					* vec.Y - Y * vec.X);
 		}
 
 		@Override
 		public Vec3.d normal() {
 			double mag = mag();
-			return new Vec3.d(X/mag, Y/mag, Z/mag);
+			return new Vec3.d(X / mag, Y / mag, Z / mag);
 		}
-				
+
 		@Override
 		public Double mag() {
-			return X*X+Y*Y+Z*Z;
+			return X * X + Y * Y + Z * Z;
 		}
-		
+
 		@Override
 		public Double length() {
-			return (double)VMath.sqrt(mag());
+			return (double) VMath.sqrt(mag());
 		}
 
 		@Override
 		public Vec3.d clone() {
 			return new Vec3.d(X, Y, Z);
 		}
-		
+
 	}
-	
-	
+
 	public N X;
 	public N Y;
 	public N Z;
-	
+
 	public Vec3() {
 	}
-	
-	public Vec3(N x, N y, N z){
+
+	public Vec3(N x, N y, N z) {
 		X = x;
 		Y = y;
 		Z = z;
 	}
-	
-	public void set(N x, N y, N z){
+
+	public Vec3(N xyz) {
+		X = xyz;
+		Y = xyz;
+		Z = xyz;
+	}
+
+	public void set(N x, N y, N z) {
 		X = x;
 		Y = y;
 		Z = z;
 	}
-	
+
+	/** Multiplies both X, Y and Z by a. */
 	public abstract Vec3<N> scale(N a);
 
 	public abstract Vec3<N> add(Vec3<N> vec);
 
 	public abstract Vec3<N> sub(Vec3<N> vec);
-	
+
 	public abstract Vec3<N> mul(Vec3<N> vec);
-	
+
 	public abstract Vec3<N> div(Vec3<N> vec);
-	
+
 	public abstract N dist(Vec3<N> vec);
-	
+
 	public abstract N dot(Vec3<N> vec);
-	
+
 	public abstract Vec3<N> cross(Vec3<N> vec);
-	
+
 	public abstract Vec3<N> normal();
-	
+
 	public abstract N mag();
-		
+
 	public abstract N length();
 
 	public abstract Vec3<N> clone();
-	
-	
+
 	@Override
-	public int hashCode(){
+	public int hashCode() {
 		return 0;
 	}
 
 	@Override
-	public String toString(){
-		return String.format("[%s, %s, %s]", X, Y, Z);
-	}
-	
-	@Override
-	public boolean equals(Object obj){
-		Vec3<?> vec = (obj instanceof Vec3<?> ? (Vec3<?>)obj : null);
+	public boolean equals(Object obj) {
+		Vec3<?> vec = (obj instanceof Vec3<?> ? (Vec3<?>) obj : null);
 		return (vec == null ? null : (X == vec.X && Y == vec.Y && Z == vec.Z));
 	}
 
-	
-	public static Vec3.i floor(Vec3.d vec){
-		return new Vec3.i((int)VMath.floor(vec.X), (int)VMath.floor(vec.Y), (int)VMath.floor(vec.Z));
+	@Override
+	public String toString() {
+		return String.format("[%s, %s, %s]", X, Y, Z);
 	}
-	
-	public static Vec3.i floor(Vec3.f vec){
-		return new Vec3.i((int)VMath.floor(vec.X), (int)VMath.floor(vec.Y), (int)VMath.floor(vec.Z));
+
+	public static Vec3.i floor(Vec3.d vec) {
+		return new Vec3.i((int) VMath.floor(vec.X), (int) VMath.floor(vec.Y),
+				(int) VMath.floor(vec.Z));
 	}
-	
+
+	public static Vec3.i floor(Vec3.f vec) {
+		return new Vec3.i((int) VMath.floor(vec.X), (int) VMath.floor(vec.Y),
+				(int) VMath.floor(vec.Z));
+	}
+
 }
